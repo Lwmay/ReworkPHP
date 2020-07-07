41c40b1168498058e348c79e43d0365d61b3287b
lmay <may.laurent@gmail.com>
2020-07-07 08:27:20 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+session_start();
+$_SESSION['user'] = [
+  'username' => 'John',
+  'password' => '0000'
+];
+$title = "Page d'accueil";
+require 'elements/header.php'; 
+?>
+
+<div class="starter-template">
+  <h1>Bootstrap starter template</h1>
+  <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+</div>
+
+<?php require 'elements/footer.php'; ?>
